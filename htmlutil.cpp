178d4c9a9a83e21dca357fd96a5cd71bd0fbb864
Tom Isaacson <tom.isaacson@teknique.com>
2023-12-07 12:32:27 +1300

    Fix TiXmlNode enum.

--- htmlutil.cpp
+++ htmlutil.cpp
@@ -56,11 +56,11 @@ void v_out_html (FILE* Fp_out,			///< Output HTML file
   {
     switch (XNp_child->Type ())
     {
-      case TiXmlNode::DOCUMENT :
+      case TiXmlNode::TINYXML_DOCUMENT :
 	fprintf (Fp_out, "\nStart document\n");
 	break;
 
-      case TiXmlNode::ELEMENT :
+      case TiXmlNode::TINYXML_ELEMENT :
 	v_levelize ((int) u_level, Fp_out, true);
 	fprintf (Fp_out, "&lt;");   // '<'
 	fprintf (Fp_out, "%s", XNp_child->ToElement ()->Value ());
@@ -77,14 +77,14 @@ void v_out_html (FILE* Fp_out,			///< Output HTML file
 	  fprintf (Fp_out, " /&gt;<br>\n");
 	break;
 
-      case TiXmlNode::COMMENT :
+      case TiXmlNode::TINYXML_COMMENT :
 	fprintf (Fp_out, "&lt;!-- %s --&gt;<br>\n", XNp_child->ToComment ()->Value ());
 	break;
-      case TiXmlNode::TEXT :
+      case TiXmlNode::TINYXML_TEXT :
 	fprintf (Fp_out, "%s\n", XNp_child->ToText ()->Value ());
 	break;
-      case TiXmlNode::UNKNOWN :
-      case TiXmlNode::DECLARATION :
+      case TiXmlNode::TINYXML_UNKNOWN :
+      case TiXmlNode::TINYXML_DECLARATION :
 	break;
       default :
 	assert (false);
@@ -94,10 +94,10 @@ void v_out_html (FILE* Fp_out,			///< Output HTML file
 
     switch (XNp_child->Type ())
     {
-      case TiXmlNode::DOCUMENT :
+      case TiXmlNode::TINYXML_DOCUMENT :
 	fprintf (Fp_out, "\nEnd document\n");
 	break;
-      case TiXmlNode::ELEMENT :
+      case TiXmlNode::TINYXML_ELEMENT :
 	if (XNp_child->FirstChild ())
 	{
 	  v_levelize ((int) u_level, Fp_out, true);
@@ -106,10 +106,10 @@ void v_out_html (FILE* Fp_out,			///< Output HTML file
 	  fprintf (Fp_out, "&gt;<br>\n");   // '>\n'
 	}
 	break;
-      case TiXmlNode::COMMENT :
-      case TiXmlNode::TEXT :
-      case TiXmlNode::UNKNOWN :
-      case TiXmlNode::DECLARATION :
+      case TiXmlNode::TINYXML_COMMENT :
+      case TiXmlNode::TINYXML_TEXT :
+      case TiXmlNode::TINYXML_UNKNOWN :
+      case TiXmlNode::TINYXML_DECLARATION :
       default :
 	break;
     }
--- xpath_processor.cpp
+++ xpath_processor.cpp
@@ -1191,7 +1191,7 @@ void xpath_processor ::v_execute_step (int &i_relative_action,	 ///< Path positi
 	      XNp_next = XNp_father->PreviousSibling ();
 	      while (XNp_next)
 	      {
-		if (XNp_next->Type () == TiXmlNode::ELEMENT)
+		if (XNp_next->Type () == TiXmlNode::TINYXML_ELEMENT)
 		  ns_target.v_add_node_in_set_if_name_or_star (XNp_next, S_name);
 		XNp_next = XNp_next->PreviousSibling ();
 	      }
@@ -1233,7 +1233,7 @@ void xpath_processor ::v_execute_step (int &i_relative_action,	 ///< Path positi
 	      XNp_next = XNp_father->FirstChild ();
 	      while (XNp_next)
 	      {
-		if (XNp_next->Type () == TiXmlNode::COMMENT)
+		if (XNp_next->Type () == TiXmlNode::TINYXML_COMMENT)
 		  ns_target.v_add_node_in_set (XNp_next);
 		XNp_next = XNp_next->NextSibling ();
 	      }
@@ -1242,7 +1242,7 @@ void xpath_processor ::v_execute_step (int &i_relative_action,	 ///< Path positi
 	      XNp_next = XNp_father->FirstChild ();
 	      while (XNp_next)
 	      {
-		if (XNp_next->Type () == TiXmlNode::TEXT)
+		if (XNp_next->Type () == TiXmlNode::TINYXML_TEXT)
 		  ns_target.v_add_node_in_set (XNp_next);
 		XNp_next = XNp_next->NextSibling ();
 	      }
@@ -1735,7 +1735,7 @@ void xpath_processor::v_function_text (unsigned u_nb_arg,	       ///< Nb of argu
   XNp_child = XEp_context->FirstChild ();
   while (XNp_child)
   {
-    if (XNp_child->Type () == TiXmlNode::TEXT)
+    if (XNp_child->Type () == TiXmlNode::TINYXML_TEXT)
       S_res += XNp_child->Value ();
     XNp_child = XNp_child->NextSibling ();
   }
